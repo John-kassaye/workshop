94e08552c150698249f7df0f5fb853413e85f2ee
Yohannes Kassaye <jmule1567@gmail.com>
2025-05-15 14:17:17 -0700

    Adding car dealership project to workshop folder

---
+++ java-workshops/src/main/java/CarDealership/Program.java
@@ -0,0 +1,8 @@
+package CarDealership;
+
+    public class Program {
+        public static void main(String[] args) {
+            UserInterface userInterface = new UserInterface();
+            userInterface.display();
+        }
+    }
